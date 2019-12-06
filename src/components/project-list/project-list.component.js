a74f718b84f924cc0493c25be9e014836c541993
Abdul <diazabdulm@gmail.com>
2019-12-06 00:02:49 -0500

    project add and retrieval from firebase database fully works

--- src/components/project-list/project-list.component.js
+++ src/components/project-list/project-list.component.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from "react";
 import { useSelector } from "react-redux";
-import { isLoaded, isEmpty } from "react-redux-firebase/lib/utils";
+import { isLoaded } from "react-redux-firebase";
 import { Link } from "react-router-dom";
 import { ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
 import { List as ListIcon } from "@material-ui/icons";
@@ -8,21 +8,19 @@ import { List as ListIcon } from "@material-ui/icons";
 const ProjectList = () => {
   const projects = useSelector(state => state.firestore.ordered.projects);
 
-  if (!isLoaded(projects) || isEmpty(projects)) {
-    return null;
-  }
-
   return (
-    <Fragment>
-      {projects.map(({ id, text }) => (
-        <ListItem button key={id} component={Link} to={`/projects/${id}`}>
-          <ListItemIcon>
-            <ListIcon />
-          </ListItemIcon>
-          <ListItemText primary={text} />
-        </ListItem>
-      ))}
-    </Fragment>
+    isLoaded(projects) && (
+      <Fragment>
+        {projects.map(({ id, name }) => (
+          <ListItem button key={id} component={Link} to={`/projects/${id}`}>
+            <ListItemIcon>
+              <ListIcon />
+            </ListItemIcon>
+            <ListItemText primary={name} />
+          </ListItem>
+        ))}
+      </Fragment>
+    )
   );
 };
 
