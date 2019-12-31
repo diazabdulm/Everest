0ea51c0fd75157717cb44e00e2a6c2a23e1f2955
Abdul <diazabdulm@gmail.com>
2019-12-31 02:14:06 -0500

    used system fonts instead of Roboto

---
+++ src/common/theme.js
@@ -0,0 +1,23 @@
+import { createMuiTheme } from "@material-ui/core";
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      "-apple-system",
+      "system-ui",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"'
+    ].join(",")
+  }
+});
+
+export default theme;
+
+const themea =
+  "-apple-system,system-ui,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Fira Sans,Ubuntu,Oxygen,Oxygen Sans,Cantarell,Droid Sans,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Lucida Grande,Helvetica,Arial,sans-serif";
--- src/index.js
+++ src/index.js
@@ -2,18 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import CssBaseline from "@material-ui/core/CssBaseline";
+import { MuiThemeProvider, CssBaseline } from "@material-ui/core";
 
 import store from "./redux";
+import theme from "./common/theme";
 
 import App from "./components/App";
 
 ReactDOM.render(
-   <Provider store={store}>
-      <BrowserRouter>
-         <CssBaseline />
-         <App />
-      </BrowserRouter>
-   </Provider>,
-   document.getElementById("root")
+	<Provider store={store}>
+		<BrowserRouter>
+			<MuiThemeProvider theme={theme}>
+				<CssBaseline />
+				<App />
+			</MuiThemeProvider>
+		</BrowserRouter>
+	</Provider>,
+	document.getElementById("root")
 );
