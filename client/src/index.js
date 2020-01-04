67bdd9453570c6ebbad2c00ae86f5e47410fdcf0
Abdul <diazabdulm@gmail.com>
2020-01-04 02:48:28 -0800

    implemented redux-persist config for a less jarring experience. Before this implementation, because the checkUser function isn't pure (that is, an asynchronous action takes place, this is also known as side-effects) the user had to wait in the login screen until his/her account was fetched from Firebase and added to the user reducer in Redux. This resulted in a jarring experience in which if a logged in user would refresh the app, they would wait a couple of seconds in the login page and then be redirected to the todo list page. With redux-persist because it would fetch this data from the background (and simultaneous delay loading of components) the user needs not follow through such an ugly and confusing process to re-enter their account

--- client/src/index.js
+++ client/src/index.js
@@ -1,25 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import { BrowserRouter } from "react-router-dom";
 import { MuiThemeProvider, CssBaseline } from "@material-ui/core";
-import store from "./redux";
-import theme from "./common/theme";
 
+import { store, persistor } from "./redux";
+import theme from "./common/theme";
 import * as serviceWorker from "./serviceWorker.js";
 
 import App from "./components/App";
 
 ReactDOM.render(
-	<Provider store={store}>
-		<BrowserRouter>
-			<MuiThemeProvider theme={theme}>
-				<CssBaseline />
-				<App />
-			</MuiThemeProvider>
-		</BrowserRouter>
-	</Provider>,
-	document.getElementById("root")
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <MuiThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+        </MuiThemeProvider>
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>,
+  document.getElementById("root")
 );
 
 serviceWorker.register();
--- client/src/redux/index.js
+++ client/src/redux/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from "redux";
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import logger from "redux-logger";
 
 import tasks from "./tasksSlice";
@@ -10,24 +12,31 @@ import user from "./userSlice";
 const middlewares = [];
 
 if (process.env.NODE_ENV === "development") {
-	middlewares.push(logger);
+  middlewares.push(logger);
 }
 
+const persistConfig = {
+  key: "root",
+  storage
+};
+
 const rootReducer = combineReducers({
-	tasks,
-	projects,
-	visibilityFilter,
-	user
+  tasks,
+  projects,
+  visibilityFilter,
+  user
 });
 
-const store = configureStore({
-	reducer: rootReducer,
-	middleware: [
-		...getDefaultMiddleware({
-			serializableCheck: false
-		}),
-		...middlewares
-	]
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: [
+    ...getDefaultMiddleware({
+      serializableCheck: false
+    }),
+    ...middlewares
+  ]
 });
 
-export default store;
+export const persistor = persistStore(store);
